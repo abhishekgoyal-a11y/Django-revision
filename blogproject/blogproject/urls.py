e0d0b9d1418c7bc8c5371ae755e6e6790747d615
= <goyal.kumarabhishek@gmail.com>
2020-08-28 09:00:40 +0530

    add some changes

--- blogproject/blogproject/urls.py
+++ blogproject/blogproject/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('', include('blogapp.urls')),
     path('api/', include('blogapp.API.urls')),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
