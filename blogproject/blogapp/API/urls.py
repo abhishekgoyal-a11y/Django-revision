1190a01cd482227a109d9274a6053862a5c44606
= <goyal.kumarabhishek@gmail.com>
2020-09-03 12:41:33 +0530

    url/apikey

--- blogproject/blogapp/API/urls.py
+++ blogproject/blogapp/API/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('put_blog/<str:title>', put_blog, name="put_blog"),
     path('delete_blog/<str:title>', delete_blog, name="delete_blog"),
     path('pagination/', pagination.as_view(), name="pagination"),
+    path('get_blog_api/<str:api_key>', get_blog_api, name="get_blog_api"),
+    path('create_blog_api/<str:api_key>', create_blog_api, name="create_blog_api"),
+    path('get_api/', get_api, name="get_api")
 ]
--- blogproject/blogapp/API/views.py
+++ blogproject/blogapp/API/views.py
@@ -7,7 +7,11 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.generics import ListAPIView
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.authtoken.models import Token
 
+
+
+from django.contrib.auth.models import User,auth
 from ..models import Blog
 from .serializers import BlogSerializer
 
@@ -28,18 +32,16 @@ def get_blog(request, title=None):
 
 @api_view(['POST'])
 def create_blog(request):
-    if request.user.is_superuser:
-        if request.method == "POST":
-            serialize = BlogSerializer(data=request.data)
-            if serialize.is_valid():
-                serialize.save()
-                return Response(serialize.data, status=status.HTTP_201_CREATED)
-            else:
-                return Response(serialize.errors, status=status.HTTP_404_BAD_REQUEST)
-    else:
-        return Response({
-            "Response": "not have permission to create"
-        })
+    if request.method == "POST":
+        serialize = BlogSerializer(data=request.data)
+        if serialize.is_valid():
+            blog=serialize.save()
+            blog.author=request.user
+            blog.save()
+            return Response(serialize.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serialize.errors, status=status.HTTP_404_BAD_REQUEST)
+
 
 
 @api_view(['PUT'])
@@ -47,7 +49,6 @@ def create_blog(request):
 def put_blog(request, title):
     if request.method == "PUT":
         blog = Blog.objects.get(title=title)
-
         user = request.user
         if blog.author != user:
             return Response({
@@ -85,3 +86,43 @@ class pagination(ListAPIView):
     pagination_class = PageNumberPagination
     filter_backends = (SearchFilter, OrderingFilter)
     search_fields = ('title', 'description')
+
+
+
+@api_view(['GET'])
+def get_blog_api(request, api_key):
+    if request.method == "GET":
+        try:
+            username=Token.objects.get(key=api_key).user
+            username_id=User.objects.get(username=username).id
+            blogs=Blog.objects.filter(author=username_id)
+            serialize = BlogSerializer(blogs, many=True)
+            return Response(serialize.data)
+        except:
+            return Response({"error":"no api found"})
+
+
+@api_view(['POST'])
+def create_blog_api(request,api_key):
+    if request.method == "POST":
+        serialize = BlogSerializer(data=request.data)
+        if serialize.is_valid():
+            blog=serialize.save()
+            blog.author=Token.objects.get(key=api_key).user
+            blog.save()
+            return Response(serialize.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serialize.errors, status=status.HTTP_404_BAD_REQUEST)
+
+
+
+@api_view(['POST'])
+def get_api(request):
+    if request.method=="POST":
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            return Response({"Api Key":Token.objects.get(user=user).key})
+        else:
+            return Response({"error":"user is not found"})
